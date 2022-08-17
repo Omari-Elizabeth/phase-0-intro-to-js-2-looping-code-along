390b6467cae79749b0bc7116e498c8c8160a8ea5
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-08-17 17:37:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for(let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts;
+}
+function writeCards(names, event){
+    
+    let newArray = [];
+    for (let i=0; i < names.length; i++){
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(){
+    let n = 10;
+    while (n >= 0){
+        console.log (n);
+        n--;
+    };
+};
+countDown();
